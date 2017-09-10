e77ff7cd9171dc94f2b1c3353f05eab58357981b
Ali Najafizadeh <a.najafizadeh@gmail.com>
2017-09-09 20:46:16 -0400

    fixed a typo

--- React/Base/RCTJavaScriptLoader.mm
+++ React/Base/RCTJavaScriptLoader.mm
@@ -246,7 +246,7 @@ static void attemptAsynchronousLoadOfBundleAtURL(NSURL *scriptURL, RCTSourceLoad
 
     // Validate that the packager actually returned javascript.
     NSString *contentType = headers[@"Content-Type"];
-    NSString *mimeType = [[contentType componentSeparatedByString:@";"] firstObject];
+    NSString *mimeType = [[contentType componentsSeparatedByString:@";"] firstObject];
     if (![mimeType isEqualToString:@"application/javascript"] &&
         ![mimeType isEqualToString:@"text/javascript"]) {
       NSString *description = [NSString stringWithFormat:@"Expected MIME-Type to be 'application/javascript' or 'text/javascript', but got '%@'.", mimeType];
